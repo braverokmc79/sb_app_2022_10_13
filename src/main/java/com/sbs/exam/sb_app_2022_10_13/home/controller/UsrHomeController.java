c664e1d775d2ef0f587632eccc14e0fc42a690d5
braverokmc79 <braverokmc79@gmail.com>
2024-01-03 22:41:41 +0900

    22강, count 조절 액션 구현

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/home/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/home/controller/UsrHomeController.java
@@ -8,6 +8,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UsrHomeController {
 
+
+  private int count;
+
+  public UsrHomeController(){
+     count=0;
+  }
+
+
   @RequestMapping("/user/home/main")
   @ResponseBody
   public String showMain(){
@@ -15,10 +23,25 @@ public class UsrHomeController {
   }
 
 
+
   @RequestMapping("/user/home/show1")
   @ResponseBody
   public String show1(){
     return "안녕하세요1";
+  }
+
+  @RequestMapping("/user/home/getCount")
+  @ResponseBody
+  public int getCount(){
+    return count;
+  }
+
+
+  @RequestMapping("/user/home/doSetCount")
+  @ResponseBody
+  public String doSetCount(int count){
+    this.count=count;
+    return "count  값이 : "+count+"로 초기화 되었습니다.";
   }
 
 
